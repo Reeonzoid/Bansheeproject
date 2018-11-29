6bcb119ca1c97f99d0ee2567bb287bc8325e2bbc
Rian <riandebruijn@gmail.com>
2018-11-29 20:49:18 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <div class="banner__container">
+    <img src="~/assets/TheBansheesLogo_Mythallic.png" class="banner__logo">
+    <ul id="nav-list">
+      <li v-for="item in items">
+      {{item.message}}
+      </li>
+    </ul>
+  </div>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+var menulist = new Vue({
+  el: '#nav-list',
+  data: {
+    items: [
+      {message: 'About us'},
+      {message: 'Tournaments'},
+      {message: 'Twitch'},
+      {message: 'contact'}
+    ]
+  }
+});
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+
+    <div class="test"></div>
+
+  </section>
+</template>
+
+<script>
+export default {
+layout: 'default'
+}
+
+</script>
